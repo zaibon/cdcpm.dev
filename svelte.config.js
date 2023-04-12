a5b5bd34e3e3fdcf7766f26b0c7dfff2a82ef2ca
Christophe de Carvalho <christophe.dcpm@gmail.com>
2023-04-12 22:16:13 +0100

    configure static building

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +10,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true,
+			strict: true
+		})
 	}
 };
 
