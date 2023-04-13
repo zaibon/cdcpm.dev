c509fb84f94b7e0c9c83e6a51a56accfedc0682e
Christophe de Carvalho <christophe.dcpm@gmail.com>
2023-04-14 00:25:13 +0100

    feat: add social meta

---
+++ src/lib/data/social.ts
@@ -0,0 +1,6 @@
+export const socialMeta = {
+	title: 'Christophe de Carvalho Pereira Martins portfolio',
+	description: 'Resume and projects I worked on as a software engineer',
+	siteURL: 'https://cdcpm.dev',
+	twitterHandler: '@zaibon'
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
-<script>
+<script lang="ts">
 	import Navbar from '../components/Navbar.svelte';
 	import Footer from '../components/Footer.svelte';
+	import { socialMeta as data } from '$lib/data/social';
 </script>
+
+<svelte:head>
+	<!-- Open Graph meta tags -->
+	<meta property="og:title" content={data.title} />
+	<meta property="og:description" content={data.description} />
+	<!-- <meta property="og:image" content="URL to your website's preview image" /> -->
+	<meta property="og:url" content={data.siteURL} />
+	<meta property="og:type" content="website" />
+
+	<!-- Twitter Card meta tags -->
+	<!-- <meta name="twitter:card" content="summary_large_image" /> -->
+	<meta name="twitter:title" content={data.title} />
+	<meta name="twitter:description" content={data.description} />
+	<!-- <meta name="twitter:image" content="URL to your website's preview image" /> -->
+	<meta name="twitter:site" content={data.twitterHandler} />
+</svelte:head>
 
 <Navbar />
 <slot />
