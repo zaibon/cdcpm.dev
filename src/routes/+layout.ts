4f710983f3bb5064a0dc5cdfee9d06a6f0cd60e6
Christophe de Carvalho <zaibon@users.noreply.github.com>
2023-06-29 23:47:15 +0200

    enable vercel analytics (#11)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,10 @@
 import type { LayoutLoad } from './$types';
 
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
+
 export const load = (async () => {
 	return {
 		headers: {
