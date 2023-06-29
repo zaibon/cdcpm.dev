97baa71899507e38bee61453e3627bf7912d859c
Christophe de Carvalho <christophe@archipelo.co>
2023-06-29 23:27:37 +0200

    migrate to vercel hosting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,29 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'plugin:svelte/prettier',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser'
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: "tsconfig.json",
+    	extraFileExtensions: [".svelte"],
 	},
 	env: {
 		browser: true,
--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,12 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
-
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true,
-			strict: true
-		})
-	}
+export default {
+    preprocess: [vitePreprocess()],
+    kit: {
+        adapter: adapter({
+            // see the 'Deployment configuration' section below
+        })
+    },
 };
-
-export default config;
