d6ca8d35f2a5bd0d7e0d2ffe4e7c3d45ac5b877b
Christophe de Carvalho <christophe@archipelo.co>
2023-09-07 00:32:55 +0100

    enable debuging in vscode

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	let plugins = [sveltekit()];
+	if (mode === 'development') {
+		plugins = [nodeLoaderPlugin(), ...plugins];
 	}
+	return {
+		plugins: plugins,
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
+	};
 });
